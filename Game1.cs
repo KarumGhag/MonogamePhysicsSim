76fc6503d06d2ed5f56ed6a5edcb9a0b3865139b
KarumGhag <karumghag09@gmail.com>
2025-08-01 16:10:18 +0100

    init

---
+++ Game1.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml.Serialization;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonogamePhysicsSim;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _circle;
+
+    private Vector2 _center;
+
+
+
+    
+
+
+
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        int screenWidth = GraphicsDevice.Viewport.Width;
+        int screenHeight = GraphicsDevice.Viewport.Height;
+
+        _center = new Vector2(screenWidth / 2, screenHeight / 2);
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _circle = Content.Load<Texture2D>("circle");
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(_circle, _center, Color.White);
+
+        _spriteBatch.End();
+        
+
+        base.Draw(gameTime);
+    }
+}
---
+++ _entityClass/Entity.cs
@@ -0,0 +1,9 @@
+using MonogamePhysicsSim;
+
+namespace Entity
+{
+    public class Entity
+    {
+        
+    }
+}
