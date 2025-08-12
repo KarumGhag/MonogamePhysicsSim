0374a981766c4135da251b9fe08e2cbe1d5d051d
KarumGhag <karumghag09@gmail.com>
2025-08-12 19:17:21 +0100

    merge test

--- obj/Debug/net8.0/MonogamePhysicsSim.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonogamePhysicsSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonogamePhysicsSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc531ff15d6feae30076f6f633c94f53118034a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5aaef9d136c4ab1a856d0de0834e875ff4e4ca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonogamePhysicsSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonogamePhysicsSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
